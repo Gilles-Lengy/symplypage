9431a05d8ff7705e154340b2517871d3b2e570d4
Gilles Lengy <gilles.lengy@gmail.com>
2020-05-22 10:50:43 +0200

    Getting pageTitle from a jason file

--- src/Controller/SymplypageController.php
+++ src/Controller/SymplypageController.php
@@ -6,6 +6,8 @@ namespace App\Controller;
 
 use App\Model\Symplypage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
@@ -19,25 +21,19 @@ class SymplypageController extends AbstractController
 
     public function homepage()
     {
+        $package = new Package( new EmptyVersionStrategy());
 
-        $symplyPage = new Symplypage();
-
-        $symplyPage->setPageTitle("Hello world from the controller !!!!");
-
-        dump($symplyPage);
+        $urlSymplyPageJson = $package->getUrl('json/symplypage.json');
+        $symplyPageJson = file_get_contents($urlSymplyPageJson);
 
         $normalizer = new ObjectNormalizer();
         $encoder = new JsonEncoder();
 
         $serializer = new Serializer([$normalizer], [$encoder]);
+        
+        $symplyPage = $serializer->deserialize($symplyPageJson, Symplypage::class, 'json');
 
-        $json = $serializer->serialize($symplyPage, 'json');
-
-        $symplyPageCopy = $serializer->deserialize($json, Symplypage::class, 'json');
-
-        dump($symplyPageCopy);
-
-        return $this->render('symplypage.html.twig',['symplyPageCopy' => $symplyPageCopy]);
+        return $this->render('symplypage.html.twig',['symplyPage' => $symplyPage]);
     }
 
 }
