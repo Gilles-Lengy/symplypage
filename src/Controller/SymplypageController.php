a394869896e362fc162738c0f0a1ff42865dcb41
Gilles Lengy <gilles.lengy@gmail.com>
2020-07-09 05:52:52 +0200

    SymplypageController.php refactored

--- src/Controller/SymplypageController.php
+++ src/Controller/SymplypageController.php
@@ -23,27 +23,11 @@ class SymplypageController extends AbstractController
      * @Route("/{emptySymplyPage}")
      */
 
-    public function homepage(int $emptySymplyPage=0, CacheInterface $cache)
+    public function homepage(int $emptySymplyPage = 0, CacheInterface $cache)
     {
+        $package = new Package(new EmptyVersionStrategy());
 
-        $package = new Package( new EmptyVersionStrategy());
-
-        $urlSymplyPageJson = $package->getUrl('json/symplypage.json');
-        $symplyPageJson = file_get_contents($urlSymplyPageJson);
-
-        $normalizer = new ObjectNormalizer();
-        $encoder = new JsonEncoder();
-
-        $serializer = new Serializer([$normalizer], [$encoder]);
-
-        if($this->getParameter('kernel.debug') || $emptySymplyPage ===1 ) {
-            $cache->delete('symplyPage');
-        }
-
-        $symplyPage = $cache->get('symplyPage', function (ItemInterface $item) use ($serializer,$symplyPageJson){
-            $item->expiresAfter(60*60*24);//24 hours
-            return $serializer->deserialize($symplyPageJson, Symplypage::class, 'json');
-        });
+        //CssPreloader
 
         $urlCssPreloaderYaml = $package->getUrl('yaml/css_preloader.yaml');
 
@@ -57,8 +41,26 @@ class SymplypageController extends AbstractController
         $cssPreloader = $serializerCssPreloaderYaml->deserialize($cssPreloaderYaml, CssPreloader::class, 'yaml');
 
 
+        // Symplypage Content
 
-        return $this->render('symplypage.html.twig',['cssPreloader' => $cssPreloader,'symplyPage' => $symplyPage]);
+        $urlSymplyPageJson = $package->getUrl('json/symplypage.json');
+        $symplyPageJson = file_get_contents($urlSymplyPageJson);
+
+        $normalizerSymplyPageJson = new ObjectNormalizer();
+        $encoderSymplyPageJson = new JsonEncoder();
+
+        $serializerSymplyPageJson = new Serializer([$normalizerSymplyPageJson], [$encoderSymplyPageJson]);
+
+        if ($this->getParameter('kernel.debug') || $emptySymplyPage === 1) {
+            $cache->delete('symplyPage');
+        }
+
+        $symplyPage = $cache->get('symplyPage', function (ItemInterface $item) use ($serializerSymplyPageJson, $symplyPageJson) {
+            $item->expiresAfter(60 * 60 * 24);//24 hours
+            return $serializerSymplyPageJson->deserialize($symplyPageJson, Symplypage::class, 'json');
+        });
+
+        return $this->render('symplypage.html.twig', ['cssPreloader' => $cssPreloader, 'symplyPage' => $symplyPage]);
     }
 
 }
